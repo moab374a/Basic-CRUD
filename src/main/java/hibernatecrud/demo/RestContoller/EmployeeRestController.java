4220bd6d6e20edbd69bd9099f4cb537f4affe4b2
moab374a <medo.1007@liv.com>
2020-11-16 18:18:31 +0100

    Rest controller methods to find and add Employee

--- src/main/java/hibernatecrud/demo/RestContoller/EmployeeRestController.java
+++ src/main/java/hibernatecrud/demo/RestContoller/EmployeeRestController.java
@@ -4,9 +4,7 @@ package hibernatecrud.demo.RestContoller;
 import hibernatecrud.demo.Service.EmployeeService;
 import hibernatecrud.demo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,31 @@ public class EmployeeRestController {
     {
         return employeeService.findAll();
     }
+
+    // add mapping for get /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId)
+    {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null) throw new RuntimeException("Employee not Exsist");
+
+        return theEmployee;
+    }
+
+    // Add mapping for Post /employees - add new employee
+
+    @PostMapping("/employees/")
+    public Employee addEmployee(@RequestBody Employee theEmployee)
+    {
+        //also just in case they pass an id in Json ... set id to 0
+        // this is to force a save of new item instead of update
+
+        theEmployee.setId(0);
+        employeeService.save(theEmployee);
+
+        return theEmployee;
+    }
+
+
 
 }
