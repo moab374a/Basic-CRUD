e253832d0db32a8f9d89992fe9fe89df0985fbe1
moab374a <medo.1007@liv.com>
2020-11-17 15:56:06 +0100

    We can specify the base path in the properties file. also controling other configutation.

--- src/main/java/SpringDataRest/Dao/EmployeeRepository.java
+++ src/main/java/SpringDataRest/Dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package SpringDataRest.Dao;
 
 import SpringDataRest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
 
     // that's it no need for implementation.
